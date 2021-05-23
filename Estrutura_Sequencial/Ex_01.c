9d9c03510594e48994629d3c43367930fc44fa1e
Matheus Gonçalves <matheus.avx@gmail.com>
2021-05-23 19:28:03 -0300

    Exercícios  01 Feito

---
+++ Estrutura_Sequencial/Ex_01.c
@@ -0,0 +1,19 @@
+/* Faça um programa que leia um número inteiro e imprima */
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+
+
+int main(){
+
+    int numero_inteiro;
+    
+    printf("Digite um Número inteiro: ");
+    scanf("%d",&numero_inteiro);
+
+    printf("O Número Digitado Foi -> %d\n",numero_inteiro);
+
+return 0;   
+
+}
