d3b863f29776921aec78246d45837091a7d39472
Matheus Gonçalves <matheus.avx@gmail.com>
2022-08-21 19:11:28 -0300

    c
    c

--- Exercícios C Curso/04.c
+++ Exercícios C Curso/04.c
@@ -1,3 +1,28 @@
 /* 4. Faça um programa que leia um número e, caso ele seja positivo, calcule e mostre:
-• O número digitado ao quadrado
-A raiz quadrada do número digitado */
+    • O número digitado ao quadrado
+    • A raiz quadrada do número digitado */
+
+#include <math.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    float numero;
+    float raiz;
+    float quadrado;
+
+    printf("Digite um NUMERO: ");
+    scanf("%f",&numero);
+
+    if (numero > 0){
+        
+        raiz = sqrt(numero);
+        quadrado = pow(numero,2);  
+
+        printf("A Raiz de %.0f -> %.0f",numero,raiz);
+        printf("\n");
+        printf("O Numero %.0f Ao Quadrado -> %.0f",numero,quadrado);
+    }
+}
--- Exercícios C Curso/05.c
+++
@@ -1,2 +0,0 @@
-/* 5. Faça um programa que receba um número inteiro e verifique se este numero é par ou
-impar.*/
