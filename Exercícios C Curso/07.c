29d98c392eeded5bdfc1c1b683b2814fdef8f235
Matheus Gonçalves <matheus.avx@gmail.com>
2022-08-21 19:20:12 -0300

    c
    c

--- Exercícios C Curso/07.c
+++ Exercícios C Curso/07.c
@@ -1,2 +1,7 @@
 /* 7. Faça um programa que receba dois números e mostre o maior. Se por acaso, os dois
 números forem iguais, imprima a mensagem Números iguais. */ 
+
+#include <math.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
