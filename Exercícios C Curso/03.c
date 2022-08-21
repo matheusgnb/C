d1bda74fe0e86715a0737c07cd942affe3fd3433
Matheus Gonçalves <matheus.avx@gmail.com>
2022-08-21 18:55:39 -0300

    commit
    commit

--- Exercícios C Curso/03.c
+++ Exercícios C Curso/03.c
@@ -7,7 +7,19 @@ imprima o numero ao quadrado. */
 #include <stdlib.h>
 
 int main(){
-
     
+    float numero;
+    float raiz;
+    float quadrado;
 
+    printf("Digite um NUMERO: ");
+    scanf("%f",&numero);
+
+    if (numero > 0){
+        raiz = sqrt(numero);
+        printf("A Raiz de %.0f -> %.0f",numero,raiz);
+    } else if (numero < 0){
+        quadrado = pow(numero,2);
+        printf("O Numero %.0f Ao Quadrado -> %.0f",numero,quadrado);
+    }
 }
