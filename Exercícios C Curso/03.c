ce60581dac9f7459b43c1c173600081e0d1dba1a
Matheus Gonçalves <matheus.avx@gmail.com>
2022-08-21 18:41:06 -0300

    Update 03.c
    commit

--- Exercícios C Curso/03.c
+++ Exercícios C Curso/03.c
@@ -1,2 +1,13 @@
 /* 3. Leia um numero real. Se o número for positivo imprima a raiz quadrada. Do contrário,
 imprima o numero ao quadrado. */ 
+
+#include <math.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+int main(){
+
+    
+
+}
