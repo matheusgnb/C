85466bce83cf4a8c0c4b9e2eae132d7437989f66
Matheus Gonçalves <matheus.avx@gmail.com>
2022-08-28 15:03:35 -0300

    Create 10.c

---
+++ Exercícios C Curso/10.c
@@ -0,0 +1,17 @@
+/* 
+
+10. Faça um programa que receba a altura e o sexo de uma pessoa e calcule e mostre seu
+peso ideal, utilizando as seguintes fórmulas (onde h corresponde à altura):
+• Homens: (72.7 * h) - 58
+• Mulheres: (62.1 * h) - 14.7
+
+*/
+
+#include <math.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+int main(){
+    
+}
